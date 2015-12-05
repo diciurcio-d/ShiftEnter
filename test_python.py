ff740de28c75caafab0786e44b8e07b8b5f581bf
David DiCiurcio <daviddiciurcio@Galvatron.local>
2015-12-04 20:29:48 -0500

    copying loose code in to test_python.py

--- test_python.py
+++ test_python.py
@@ -2,3 +2,7 @@ import numpy as np
 
 def sigmoidfun(x):
 	return 1/(1+np.exp(-0.007*(x-800)))
+
+merged_df = pd.merge(df85_15,df85_15.groupby(["PLAYER_NAME",'OPP']).apply(lambda x: x.FANTASY_PTS).reset_index())#.rename(columns = {0:"FANTASY_PTS_AGAINST"}), left_on=['PLAYER_NAME','OPP'],right_on=['PLAYER_NAME','OPP'])
+
+high_rollers = testdata.groupby(["PLAYER_NAME",'OPP']).apply(lambda x: rolling_cols(x,['FANTASY_PTS'],3,'mean'))
